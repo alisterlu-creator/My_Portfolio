b6d27d3490f020121c0ad0f7c330d849c7d7409e
Alister <alisterlugstein@gmail.com>
2026-03-03 12:35:14 +0100

    creats project typescript with project array

---
+++ myprojects.ts
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    id: 1,
+    projectName: 'Name of my projecz',
+    image: '../images/task1.jpg',
+    projectDescription: 'This is what this is',
+  },
+  {
+    id: 2,
+    projectName: 'Name of my projecz',
+    image: '../images/task1.jpg',
+    projectDescription: 'This is what this is',
+  },
+  {
+    id: 3,
+    projectName: 'Name of my projecz',
+    image: '../images/task1.jpg',
+    projectDescription: 'This is what this is',
+  },
+];
