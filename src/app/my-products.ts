67c86c9dd3eadd2030afe37d7b20052ab5b7090a
Alister <alisterlugstein@gmail.com>
2026-03-04 23:35:52 +0100

    adds pictures of my projects to projects array

--- src/app/my-products.ts
+++ src/app/my-products.ts
@@ -2,21 +2,21 @@ export const myProjects = [
   {
     id: 1,
     projectName: 'Food Blog',
-    image: '../images/task1.jpg',
+    image: '../images/food_blog.png',
     projectDescription:
       'building a responsive food blog website using semantic html and css (flexbox/grid, positioning, and media queries), including additional “recipe of the month” and “about” pages, and uploading it to a private github repository.',
   },
   {
     id: 2,
     projectName: 'Shopping List',
-    image: '../images/task1.jpg',
+    image: '../images/shopping_list.png',
     projectDescription:
       'creating a responsive shopping list web app using html, css, bootstrap, and javascript, where items are stored in an array and dynamically displayed. users can increase importance (max 5), see color changes based on priority, and sort items by importance.',
   },
   {
     id: 3,
     projectName: 'Online Shop',
-    image: '../images/task1.jpg',
+    image: 'images/famazon.png',
     projectDescription:
       'building a responsive spa with routing, product listing, details page, and cart functionality. users can add available products to the cart, and the total must include 10% service and a 15% discount for orders over €40.',
   },
