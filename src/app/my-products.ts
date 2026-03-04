59b235561bc35c8da8c5c534ea388943bfa3ba49
Alister <alisterlugstein@gmail.com>
2026-03-04 22:08:32 +0100

    adds lowercase text

--- src/app/my-products.ts
+++ src/app/my-products.ts
@@ -4,20 +4,20 @@ export const myProjects = [
     projectName: 'Food Blog',
     image: '../images/task1.jpg',
     projectDescription:
-      'Building a responsive food blog website using semantic HTML and CSS (Flexbox/Grid, positioning, and media queries), including additional “Recipe of the Month” and “About” pages, and upload it to a private GitHub repository. ',
+      'building a responsive food blog website using semantic html and css (flexbox/grid, positioning, and media queries), including additional “recipe of the month” and “about” pages, and uploading it to a private github repository.',
   },
   {
     id: 2,
     projectName: 'Shopping List',
     image: '../images/task1.jpg',
     projectDescription:
-      'Creating a responsive Shopping List web app using HTML, CSS, Bootstrap, and JavaScript, where items are stored in an array and dynamically displayed. Users can increase importance (max 5), see color changes based on priority, and sort items by importance.',
+      'creating a responsive shopping list web app using html, css, bootstrap, and javascript, where items are stored in an array and dynamically displayed. users can increase importance (max 5), see color changes based on priority, and sort items by importance.',
   },
   {
     id: 3,
     projectName: 'Online Shop',
     image: '../images/task1.jpg',
     projectDescription:
-      'Building a responsive SPA with routing, product listing, details page, and cart functionality. Users can add available products to the cart, and the total must include 10% service and a 15% discount for orders over €40',
+      'building a responsive spa with routing, product listing, details page, and cart functionality. users can add available products to the cart, and the total must include 10% service and a 15% discount for orders over €40.',
   },
 ];
