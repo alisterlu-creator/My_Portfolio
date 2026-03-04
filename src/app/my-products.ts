15412b303667efa2fcdfdc9b9f91e36e81220b86
Alister <alisterlugstein@gmail.com>
2026-03-04 22:08:14 +0100

    adds projects with descriptions

--- src/app/my-products.ts
+++ src/app/my-products.ts
@@ -1,20 +1,23 @@
 export const myProjects = [
   {
     id: 1,
-    projectName: 'Name of my project',
+    projectName: 'Food Blog',
     image: '../images/task1.jpg',
-    projectDescription: 'This is what this is',
+    projectDescription:
+      'Building a responsive food blog website using semantic HTML and CSS (Flexbox/Grid, positioning, and media queries), including additional “Recipe of the Month” and “About” pages, and upload it to a private GitHub repository. ',
   },
   {
     id: 2,
-    projectName: 'Name of my projecz',
+    projectName: 'Shopping List',
     image: '../images/task1.jpg',
-    projectDescription: 'This is what this is',
+    projectDescription:
+      'Creating a responsive Shopping List web app using HTML, CSS, Bootstrap, and JavaScript, where items are stored in an array and dynamically displayed. Users can increase importance (max 5), see color changes based on priority, and sort items by importance.',
   },
   {
     id: 3,
-    projectName: 'Name of my projecz',
+    projectName: 'Online Shop',
     image: '../images/task1.jpg',
-    projectDescription: 'This is what this is',
+    projectDescription:
+      'Building a responsive SPA with routing, product listing, details page, and cart functionality. Users can add available products to the cart, and the total must include 10% service and a 15% discount for orders over €40',
   },
 ];
