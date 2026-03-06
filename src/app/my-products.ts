272f4b945465c311cb0d883da47e99491bc9caca
Alister <alisterlugstein@gmail.com>
2026-03-06 03:15:55 +0100

    rewrites product desc

--- src/app/my-products.ts
+++ src/app/my-products.ts
@@ -4,20 +4,20 @@ export const myProjects = [
     projectName: 'Food Blog',
     image: '../images/food_blog.png',
     projectDescription:
-      'the problem was users needed a responsive food blog website, so i built a responsive site with semantic html and css using flexbox/grid layouts and media queries for mobile and tablet. technologies: html, css, javascript my contribution: created layout, positioned images with overlays, implemented responsiveness and media queries',
+      'the task was to create a responsive food blog website, so i built a responsive site with semantic html and css using flexbox/grid layouts and media queries for mobile and tablet. i used html and css. my contribution was creating a layout, positioning images, implementing responsiveness and media queries',
   },
   {
     id: 2,
     projectName: 'Shopping List',
     image: '../images/shopping_list.png',
     projectDescription:
-      'the problem was users needed an interactive shopping list that tracks item importance, so i created a dynamic app that displays tasks, allows increasing priority, and sorts items by importance with javascript and bootstrap.technologies: html, css, javascript, bootstrap my contribution: implemented dynamic rendering from array, priority buttons, color coding, and sorting functionality ',
+      'the task was to create an interactive shopping list that tracks item importance, so i created a dynamic app that displays tasks, allows increasing priority, and sorts items by importance with javascript and bootstrap.i used html, css, javascript, bootstrap. my contribution was implemening dynamic rendering from array, adding priority buttons, color coding, and adding a sorting functionality ',
   },
   {
     id: 3,
     projectName: 'Online Shop',
     image: 'images/famazon.png',
     projectDescription:
-      'the problem was users needed a single-page e-commerce application with cart functionality, so i built a responsive spa with routing, product listing, details page, and order/cart management including service charges and discounts with typescript and angular. technologies: html, css, typescript, angular my contribution: implemented routing, product and cart components, details view, and total calculation logic with discounts',
+      'the task was to create a single-page e-commerce application with cart functionality, so i built a responsive spa with routing, product listing, details page, and order/cart management including service charges and discounts with typescript and angular. i used html, css, typescript, angular. my contribution was implementing routing, making different components like product and cart, calculating the total with discount and fee.',
   },
 ];
